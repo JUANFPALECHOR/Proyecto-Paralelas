09709dbabf66555c5bc9993f1d6e15103c8d703f
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2025-11-23 22:28:16 -0500

    feat(ingestion): soporte para múltiples WARC, metadatos y CSV completo
    Se agrega:
    - Procesamiento de archivos .warc.gz mediante --file o --dir
    - Límite de páginas por archivo (--limit)
    - Extracción de título, fecha y dominio
    - Limpieza HTML mejorada con fallback automático
    - Escritura de resultados en output.csv
    - Manejo robusto de errores
    Probado con archivo real ~1GB.

--- ingestion/main.py
+++ ingestion/main.py
@@ -1,10 +1,31 @@
+import argparse
+import os
 from warc_reader import process_warc_file
 
-# archivo de ejemplo 
-WARC_PATH = "../data/CC-MAIN-20180420081400-20180420101400-00000.warc.gz"
+def main():
+    parser = argparse.ArgumentParser(description="Procesador de archivos WARC")
+    parser.add_argument("--file", type=str, help="Ruta a un archivo WARC específico")
+    parser.add_argument("--dir", type=str, help="Ruta a un directorio con varios archivos WARC")
+    parser.add_argument("--limit", type=int, default=50, help="Número de páginas por archivo")
+
+    args = parser.parse_args()
+
+    if args.file:
+        print(f"📥 Procesando archivo: {args.file}")
+        process_warc_file(args.file, limit=args.limit)
+        return
+
+
+    if args.dir:
+        print(f"📂 Procesando todos los WARC en: {args.dir}")
+        for fname in os.listdir(args.dir):
+            if fname.endswith(".warc.gz"):
+                fullpath = os.path.join(args.dir, fname)
+                print(f"\n📥 Procesando: {fullpath}")
+                process_warc_file(fullpath, limit=args.limit)
+        return
+
+    print("⚠️ Debes pasar --file o --dir")
 
 if __name__ == "__main__":
-    print("📥 Iniciando ingesta de datos...")
-    process_warc_file(WARC_PATH)
-    print("✔ Ingesta finalizada.")
-
+    main()
